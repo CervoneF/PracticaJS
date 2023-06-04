2aacac7d0fe5e591d879581aa92d045cd92ddb84
Florencia <mfc.mdp86.gmail.com>
2023-06-03 23:43:58 -0300

    Práctica Variables JS

---
+++ script.js
@@ -0,0 +1,78 @@
+// console.log("Hola mundo");
+
+//Ejercicio Variables
+
+// var uno = 3;
+// var dos = uno;
+// var uno = 5;
+
+// console.log(dos);
+
+//Ej Mensaje Con Variables
+
+// let nombre = "Florencia";
+// let edad = 36;
+// let cumpleanos = "26/09" ;
+// let ciudad = "Mar del Plata";
+// let ocupacion = "Empleada";
+// let pasatiempo = "Bailar";
+
+// console.log("Soy " + nombre + " tengo " + edad + " años y mi cumpleaños es el " + cumpleanos + ". Vivo en la ciudad de " + ciudad + ", mi ocupacón es " + ocupacion + " y mi pasatiempo es " + pasatiempo);
+
+//Ej Cantidad De Caracteres
+
+// let texto = prompt("Ingrese un texto breve para saber cuantos caracteres tiene");
+
+// console.log(texto);
+// console.log("Su texto tiene " + texto.length + " caracteres");
+
+//Ej Calculadora de Edad en días
+
+// let edad = parseInt(prompt("Ingrese su edad"));
+// calculadoraEdadDias= edad * 365;
+// console.log ("La edad en días es " + calculadoraEdadDias);
+
+//Ej Suma de valores
+
+// let num1 = parseFloat(prompt("Ingrese un número a sumar"));
+// let num2 = parseFloat(prompt("Ingrese otro número a sumar"));
+// let resultado = num1 + num2;
+// console.log("El resultado es " + resultado);
+
+//Ej 1 Calculador De Abastecimiento De Por Vida
+
+// let edad = 36;
+// let edadMax = 90;
+
+// let snack= "Papas fritas";
+// let snackXdia = 10;
+
+// let snackXcomer = (edadMax - edad)*365*snackXdia;
+
+// alert("Necesitas " + snackXcomer + " para que te alcancen hasta los " + edadMax);
+
+// let precioSnack = 3;
+
+// let gastoSnack = precioSnack * snackXcomer;
+
+// alert("Gastarás $" + gastoSnack + " en snacks lo que te resta de vida");
+
+//Ej 2 Calculador De Abastecimiento De Por Vida
+
+// let diasDeViaje = 20;
+
+// let presupuesto = 100000;
+
+// let cantDeComidas = 3;
+
+// let comida= presupuesto / diasDeViaje / cantDeComidas;
+
+// alert("Podés gastar " + comida +  " en cada comida para que te alcance la plata durante los " +  diasDeViaje + " días de viaje")
+
+
+
+
+
+
+
+
